1ab351555ccf29decd21050216f7ba49dddf9b61
Choong Yee Ching <rainchoong@gmail.com>
2018-09-02 13:36:40 +0800

    Initial Commit

---
+++ BullsCows/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BullsCows
+//
+//  Created by Choong Yee Ching on 02/09/2018.
+//  Copyright © 2018 Choong Yee Ching. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
